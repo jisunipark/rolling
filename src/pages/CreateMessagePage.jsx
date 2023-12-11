174433e11e170be3c2e496a656c782ab9ff62d7f
박지선 <shanshan@MacBook-Pro-7.local>
2023-12-12 01:21:58 +0900

    feat: 토글 드롭다운 옵션 디자인 작업

--- src/pages/CreateMessagePage.jsx
+++ src/pages/CreateMessagePage.jsx
@@ -1,11 +1,11 @@
 import styled from 'styled-components';
+import ToggleArrow from '../assets/images/toggle-arrow-down.svg';
 
 // 지선
 
 const StyledContainer = styled.div`
-  margin: 0 auto;
+  margin: 47px auto 62px auto;
   width: 720px;
-  height: 944px;
   background-color: white;
   display: flex;
   flex-direction: column;
@@ -25,7 +25,10 @@ const StyledSection = styled.section`
     line-height: 36px;
   }
 
-  & input {
+  & input,
+  & textarea,
+  & button {
+    padding: 12px 16px;
     border-radius: 8px;
     border: 1px solid #ccc;
   }
@@ -33,12 +36,42 @@ const StyledSection = styled.section`
   & input:focus {
     border: 2px solid #555;
   }
+
+  & input::placeholder,
+  & button {
+    color: #555;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 26px;
+    text-align: left;
+  }
+
+  & button {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 320px;
+    background-color: #fff;
+  }
+
+  & li {
+    padding: 12px 16px;
+    color: #555;
+  }
+
+  & ul {
+    width: 320px;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    border: 1px solid #ccc;
+    border-radius: 8px;
+  }
 `;
 
 const StyledButton = styled.button`
   display: flex;
   justify-content: center;
-  margin: 0 auto;
+  margin: 0 auto 24px;
   padding: 14px 24px;
   width: 720px;
   border-radius: 12px;
@@ -53,7 +86,7 @@ const CreateMessagePage = () => {
       <StyledContainer>
         <StyledSection>
           <label htmlFor="sender">From.</label>
-          <input type="text" id="sender" />
+          <input type="text" id="sender" placeholder="이름을 입력해 주세요." />
         </StyledSection>
 
         <StyledSection>
@@ -65,12 +98,16 @@ const CreateMessagePage = () => {
         <StyledSection>
           <label htmlFor="relationship">상대와의 관계</label>
 
-          <select id="relationship">
-            <option value="friend">친구</option>
-            <option value="acquaintance">지인</option>
-            <option value="coworker">동료</option>
-            <option value="family">가족</option>
-          </select>
+          <button type="button">
+            지인
+            <img src={ToggleArrow} alt="드롭다운 화살표 이미지" />
+          </button>
+          <ul>
+            <li>친구</li>
+            <li>지인</li>
+            <li>동료</li>
+            <li>가족</li>
+          </ul>
         </StyledSection>
 
         <StyledSection>
@@ -80,12 +117,16 @@ const CreateMessagePage = () => {
 
         <StyledSection>
           <label htmlFor="font">폰트 선택</label>
-          <select id="font">
-            <option value="noto-sans">Noto Sans</option>
-            <option value="pretendard">Pretendard</option>
-            <option value="나눔명조">나눔명조</option>
-            <option value="나눔손글씨-손편지체">나눔손글씨 손편지체</option>
-          </select>
+          <button type="button">
+            Noto Sans
+            <img src={ToggleArrow} alt="드롭다운 화살표 이미지" />
+          </button>
+          <ul>
+            <li>Noto Sans</li>
+            <li>Pretendard</li>
+            <li>나눔명조</li>
+            <li>나눔손글씨 손편지체</li>
+          </ul>
         </StyledSection>
       </StyledContainer>
       <StyledButton>생성하기</StyledButton>
