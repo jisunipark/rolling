912b338ff0f055a7dc7dce7ae607c3fd1ed1c1ea
정현진 <jhj1004v@naver.com>
2023-12-11 20:15:21 +0900

    CHORE : 개행문자차이해결 및 컨피그 끄기

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,32 +4,39 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'airbnb',
-    'plugin:prettier/recommended',
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "airbnb",
+    "plugin:prettier/recommended",
   ],
   overrides: [
     {
       env: {
         node: true,
       },
-      files: ['.eslintrc.{js,cjs}'],
+      files: [".eslintrc.{js,cjs}"],
       parserOptions: {
-        sourceType: 'script',
+        sourceType: "script",
       },
     },
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
   rules: {
-    'react/react-in-jsx-scope': 'off',
-    'no-unused-vars': 'warn',
-    'react/function-component-definition': [
+    "react/react-in-jsx-scope": "off",
+    "no-unused-vars": "warn",
+    "react/function-component-definition": [
       2,
-      { namedComponents: 'arrow-function' },
+      { namedComponents: "arrow-function" },
     ],
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+    "react/prop-type": "off",
   },
 };
