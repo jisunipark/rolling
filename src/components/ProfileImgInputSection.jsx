aec19b9d9fa7c4ab8560fd33255265de722388e2
박지선 <shanshan@MacBook-Pro-7.local>
2023-12-12 17:37:19 +0900

    feat: 프로필 이미지 섹션 퍼블리싱

--- src/components/ProfileImgInputSection.jsx
+++ src/components/ProfileImgInputSection.jsx
@@ -1,10 +1,67 @@
+import styled from 'styled-components';
 import StyledSection from './StyledSection';
+import DefaultProfileImg from '../assets/images/default-profile-img.png';
+import SampleImg1 from '../assets/images/sample-profile-img-1.png';
+import SampleImg2 from '../assets/images/sample-profile-img-2.png';
+
+const ProfileImgList = styled.div`
+  display: flex;
+  gap: 4px;
+`;
+
+const RoundImg = styled.img`
+  width: 56px;
+  height: 56px;
+  border-radius: 100px;
+  border: 1px solid #eee;
+`;
+
+const FlexDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+
+  & span {
+    color: #555;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 26px;
+  }
+`;
+
+const ProfileImgInput = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 32px;
+`;
 
 const ProfileImgInputSection = ({ children }) => {
   return (
     <StyledSection>
       <label htmlFor="profileImgURL">{children}</label>
-      <input type="file" id="profileImgURL" />
+      <ProfileImgInput>
+        <img
+          src={DefaultProfileImg}
+          alt="기본 프로필 이미지"
+          width="80px"
+          height="80px"
+        />
+        <FlexDiv>
+          <span>프로필 이미지를 선택해 주세요!</span>
+          <ProfileImgList>
+            <RoundImg src={SampleImg1} alt="샘플 프로필 이미지" />
+            <RoundImg src={SampleImg2} alt="샘플 프로필 이미지" />
+            <RoundImg src={SampleImg1} alt="샘플 프로필 이미지" />
+            <RoundImg src={SampleImg2} alt="샘플 프로필 이미지" />
+            <RoundImg src={SampleImg1} alt="샘플 프로필 이미지" />
+            <RoundImg src={SampleImg2} alt="샘플 프로필 이미지" />
+            <RoundImg src={SampleImg1} alt="샘플 프로필 이미지" />
+            <RoundImg src={SampleImg2} alt="샘플 프로필 이미지" />
+            <RoundImg src={SampleImg1} alt="샘플 프로필 이미지" />
+            <RoundImg src={SampleImg2} alt="샘플 프로필 이미지" />
+          </ProfileImgList>
+        </FlexDiv>
+      </ProfileImgInput>
     </StyledSection>
   );
 };
